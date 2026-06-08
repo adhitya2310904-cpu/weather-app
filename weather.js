ad19fccee55afa776afb21c3bb86846fc8a1b612
adhitya2310904-cpu <adhitya2310904@ssn.edu.in>
2026-06-08 15:04:19 +0530

    second commit

--- weather.js
+++ weather.js
@@ -1,33 +1,28 @@
 async function getWeather() {
+  const city = document.getElementById("cityInput").value;
 
-    const city = document.getElementById("cityInput").value;
+  const apiKey = "1cdc1f31ae2c94e5199e7b0d3f9e9fbf";
 
-    const apiKey = "1cdc1f31ae2c94e5199e7b0d3f9e9fbf";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-    const url =
-      'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
+  try {
+    const response = await fetch(url);
 
-    try {
+    const data = await response.json();
 
-        const response = await fetch(url);
+    if (data.cod !== 200) {
+      document.getElementById("weatherResult").innerHTML = "City not found!";
+      return;
+    }
 
-        const data = await response.json();
-
-        if(data.cod !== 200){
-            document.getElementById("weatherResult").innerHTML =
-                "City not found!";
-            return;
-        }
-
-        document.getElementById("weatherResult").innerHTML = `
+    document.getElementById("weatherResult").innerHTML = `
             <h2>${data.name}</h2>
             <p>Temperature: ${data.main.temp}°C</p>
             <p>Humidity: ${data.main.humidity}%</p>
             <p>Weather: ${data.weather[0].description}</p>
         `;
-
-    } catch(error) {
-        document.getElementById("weatherResult").innerHTML =
-            "Error fetching weather data";
-    }
+  } catch (error) {
+    document.getElementById("weatherResult").innerHTML =
+      "Error fetching weather data";
+  }
 }
